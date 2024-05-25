a0a13e9bd54c119f692120e5a27a36e4080befff
MartinPhung <martinphung26@gmail.com>
2024-05-25 13:38:00 +0200

    added first version of fetch from CocktailDB API

--- server/server.js
+++ server/server.js
@@ -17,9 +17,59 @@ app.get('/profile/:userID', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/pages/profile/profile.html'));
 });
 
+app.get('/profile/', function (req, res) {
+    res.send("Enter a valid profile ID");
+});
+
 app.get('/recipe/:recipeID', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/pages/recipe/recipe.html'));
 });
+
+app.get('/recipe/', function (req, res) {
+    res.send("Enter a valid recipe ID");
+});
+
+
+const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(responseData => {
+        // Process the data
+        // console.log(responseData);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
+
+function fetchCocktailData(searchType, searchTerm) {
+    const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?${searchType}=${searchTerm}`;
+
+    return fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+// Example usage:
+fetchCocktailData('s','Martini')
+    .then(data => {
+        console.log(data);
+        // Process the data here
+    });
 
 app.listen(666)
 console.log("Server now listening on http://localhost:666/home")
