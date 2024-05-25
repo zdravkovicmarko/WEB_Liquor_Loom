0f09637fb1c927cfae3cbf1365b8475e23723884
MartinPhung <martinphung26@gmail.com>
2024-05-25 12:27:14 +0200

    added 4 temporary endpoints for /home, /login, /recipe/:recipeID and /profile/:userID

--- server/server.js
+++ server/server.js
@@ -3,11 +3,23 @@ const path = require('path')
 const app = express()
 
 // Serve static content in directory 'home'
-app.use(express.static(path.join(__dirname, 'pages/home')));
+app.use(express.static(path.join(__dirname, 'pages')));
 
 app.get('/home', function (req, res) {
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname, 'pages', 'home', 'home.html'));
 })
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, 'pages', 'login', 'login.html'));
+});
+
+app.get('/profile/:userID', function (req, res) {
+    res.sendFile(path.join(__dirname, 'pages', 'profile', 'profile.html'));
+});
+
+app.get('/recipe/:recipeID', function (req, res) {
+    res.sendFile(path.join(__dirname, 'pages', 'recipe', 'recipe.html'));
+});
 
 app.listen(666)
 console.log("Server now listening on http://localhost:666/home")
